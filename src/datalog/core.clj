ba88b0383e8365e3e78ecfb7f620153b53c5e5bc
Kevin Downey <redchin@gmail.com>
2012-04-16 18:08:33 -0700

    checkpoint

--- src/datalog/core.clj
+++ src/datalog/core.clj
@@ -13,10 +13,13 @@
       {a b})))
 
 (defn resolve-in [clause environment]
-  (vec (map (fn [v] (get environment v v)) clause)))
+  ((if (seq? clause) identity vec)
+   (map (fn [v] (get environment v v)) clause)))
 
 (defn get-by-attribute [db attribute]
-  (filter #(= attribute (second %)) db))
+  (if attribute
+    (filter #(= attribute (second %)) db)
+    db))
 
 (defn process-results [results vars]
   (for [r results
@@ -71,3 +74,135 @@
                        environment
                        prev)))))))))
 
+(defn g [vars query rules db]
+  (let [vars (set vars)
+        given-names (set (filter symbol? (tree-seq coll? seq query)))]
+    (if (not (every? (partial contains? given-names) vars))
+      (throw (Exception. "unknown variable"))
+      ((fn F [facts clauses environment prev]
+         (lazy-seq
+          (if (and (every? #(contains? environment %) vars)
+                   (not prev))
+            [environment]
+            (if (seq? (first clauses))
+              (if-let [rules (seq (filter #(= (first (first %))
+                                              (first (first clauses))) rules))]
+                (let [[rule-name & args] (first clauses)]
+                  (concat (apply concat (for [rule rules
+                                              :let [[rule-head & rule-body] rule
+                                                    free-rule-vars (->> (tree-seq coll? seq rule-body)
+                                                                        (filter #(and (symbol? %)
+                                                                                      (.startsWith (name %) "?")))
+                                                                        (remove (set (rest rule-head)))
+                                                                        set
+                                                                        (map (juxt identity
+                                                                                   gensym))
+                                                                        (into {}))
+                                                    rule-env (merge (zipmap (rest rule-head) args)
+                                                                    free-rule-vars)
+                                                    new-clauses (concat (map #(resolve-in % rule-env) rule-body)
+                                                                        (rest clauses))]]
+                                          (F (if (seq? (first new-clauses))
+                                               (get-by-attribute db nil)
+                                               (get-by-attribute db (second (first new-clauses))))
+                                             new-clauses
+                                             environment
+                                             nil)))
+                          (F nil nil environment prev)))
+                (if-let [p (resolve (first (first clauses)))]
+                  (if (apply p (map environment (rest (first clauses))))
+                    (F (get-by-attribute db (second (second clauses)))
+                       (rest clauses)
+                       environment
+                       prev)
+                    (F (rest facts)
+                       clauses
+                       environment
+                       prev))
+                  (throw (Exception. "failed rule"))))
+              (if (and (not (seq facts))
+                       (not prev))
+                nil
+                (if (or (not (seq facts))
+                        (not (seq clauses)))
+                  (let [e environment
+                        {:keys [facts clauses environment prev]} prev]
+                    (if (every? #(contains? e %) vars)
+                      (lazy-seq
+                       (cons (reduce
+                              (fn [m [k v]]
+                                (assoc m (keyword (subs (name k) 1)) v))
+                              {}
+                              (select-keys e vars))
+                             (F facts
+                                clauses
+                                environment
+                                prev)))
+                      (F facts
+                         clauses
+                         environment
+                         prev)))
+                  (lazy-seq
+                   (let [clause (resolve-in (first clauses) environment)
+                         fact (first facts)
+                         m (map match clause fact)
+                         pass? (and (seq m)
+                                    (every? map? m)
+                                    (= (count (set (map keys m)))
+                                       (count (set (map vals m)))))]
+                     (if pass?
+                       (let [new-env (apply merge m)]
+                         (F (get-by-attribute db (second (second clauses)))
+                            (rest clauses)
+                            (into environment new-env)
+                            {:facts (rest facts)
+                             :clauses clauses
+                             :environment environment
+                             :prev prev}))
+                       (F (rest facts)
+                          clauses
+                          environment
+                          prev)))))))
+            (if (seq? (first clauses))
+              (if-let [rules (seq (filter #(= (first (first %))
+                                              (first (first clauses))) rules))]
+                (let [[rule-name & args] (first clauses)]
+                  (concat (apply concat (for [rule rules
+                                              :let [[rule-head & rule-body] rule
+                                                    free-rule-vars (->> (tree-seq coll? seq rule-body)
+                                                                        (filter #(and (symbol? %)
+                                                                                      (.startsWith (name %) "?")))
+                                                                        (remove (set (rest rule-head)))
+                                                                        set
+                                                                        (map (juxt identity
+                                                                                   gensym))
+                                                                        (into {}))
+                                                    rule-env (merge (zipmap (rest rule-head) args)
+                                                                    free-rule-vars)
+                                                    new-clauses (concat (map #(resolve-in % rule-env) rule-body)
+                                                                        (rest clauses))]]
+                                          (F (if (seq? (first new-clauses))
+                                               (get-by-attribute db nil)
+                                               (get-by-attribute db (second (first new-clauses))))
+                                             new-clauses
+                                             environment
+                                             nil)))
+                          (F nil nil environment prev)))
+                (if-let [p (resolve (first (first clauses)))]
+                  (if (apply p (map environment (rest (first clauses))))
+                    (F (get-by-attribute db (second (second clauses)))
+                       (rest clauses)
+                       environment
+                       prev)
+                    (F (rest facts)
+                       clauses
+                       environment
+                       prev))
+                  (throw (Exception. "failed rule"))))
+              ))))
+       (if (seq? (first query))
+         (get-by-attribute db nil)
+         (get-by-attribute db (second (first query))))
+       query
+       {}
+       nil))))
--- test/datalog/test/core.clj
+++ test/datalog/test/core.clj
@@ -23,7 +23,11 @@
     [#uuid "ee3f8ced-d0ff-432b-b479-03aa7e8e0e4f" :street "Lake City Way NE"]
     [#uuid "33005ea0-e284-4b0e-b120-e4d5b828630e" :first-name "Ariella"]
     [#uuid "a99b3f8c-e81f-450d-97b6-527d5db4c877" :owners-name "Ariella"]
-    })
+    ["bill" :parent "mary"]
+    ["mary" :parent "john"]
+    ["john" :parent "jack"]
+    ["jack" :parent "george"]
+    ["tom" :parent "jack"]})
 
 (deftest t-stuff
   (is (= (f '[?p2-first-name ?p1-last-name ?p1-name ?place1]
@@ -38,4 +42,86 @@
          '({:p2-first-name "Michael",
             :p1-last-name "Downey",
             :p1-name "Bar"
-            :place1 #uuid "5d416576-9c6c-49c3-98ad-70f44b525004"}))))
+            :place1 #uuid "5d416576-9c6c-49c3-98ad-70f44b525004"})))
+
+  (is (= (f '[?p1-first-name ?p1-last-name ?p2-first-name]
+            '[[?person1 :street "Lake City Way NE"]
+              [?person1 :last-name ?p1-last-name]
+              [?person1 :first-name ?p1-first-name]
+              [?person2 :last-name ?p1-last-name]
+              [?person2 :first-name ?p2-first-name]]
+            db)
+         '({:p2-first-name "Michael",
+            :p1-last-name "Downey",
+            :p1-first-name "Kevin"}
+           {:p2-first-name "Kevin",
+            :p1-last-name "Downey",
+            :p1-first-name "Kevin"}
+           {:p2-first-name "Ariella",
+            :p1-last-name "Abelseth",
+            :p1-first-name "Ariella"})))
+
+  (is (= (set (g '[?p1-first-name ?p1-last-name ?p2-first-name]
+                 '[[?person1 :street "Lake City Way NE"]
+                   [?person1 :last-name ?p1-last-name]
+                   [?person1 :first-name ?p1-first-name]
+                   [?person2 :last-name ?p1-last-name]
+                   [?person2 :first-name ?p2-first-name]]
+                 []
+                 db))
+         (set '({:p2-first-name "Michael",
+                 :p1-last-name "Downey",
+                 :p1-first-name "Kevin"}
+                {:p2-first-name "Kevin",
+                 :p1-last-name "Downey",
+                 :p1-first-name "Kevin"}
+                {:p2-first-name "Ariella",
+                 :p1-last-name "Abelseth",
+                 :p1-first-name "Ariella"}))))
+
+  (is (= (g '[?p2-first-name ?p1-last-name ?p1-name ?place1]
+            '[[?person1 :street "Lake City Way NE"]
+              [?person1 :last-name ?p1-last-name]
+              [?person2 :last-name ?p1-last-name]
+              [?person2 :street "Hanam"]
+              [?person2 :first-name ?p2-first-name]
+              [?place1 :owners-name ?p2-first-name]
+              [?place1 :name ?p1-name]]
+            []
+            db)
+         '({:p2-first-name "Michael",
+            :p1-last-name "Downey",
+            :p1-name "Bar"
+            :place1 #uuid "5d416576-9c6c-49c3-98ad-70f44b525004"})))
+  
+  (is (= (g '[?a]
+            '[(ancestor "bill" ?a)]
+            '[[[ancestor ?X ?Y] [?X :parent ?Y]]
+              [[ancestor ?X ?Y] [?X :parent ?Z] (ancestor ?Z ?Y)]]
+            db)
+         '({:a "mary"}
+           {:a "john"}
+           {:a "jack"}
+           {:a "george"})))
+
+  (is (= (g '[?a]
+            '[(sibling "tom" ?a)]
+            '[[[sibling ?X ?Y]
+               [?X :parent ?Z]
+               [?Y :parent ?Z]]]
+            db)
+         '({:a "john"}
+           {:a "tom"})))
+
+  (is (= (g '[?fname ?f]
+            '[[?person1 :street "Lake City Way NE"]
+              (family ?person1 ?relative)
+              [?person1 :first-name ?f]
+              [?relative :first-name ?fname]
+              (not= ?f ?fname)]
+            '[[[family ?X ?Y]
+               [?X :last-name ?Z]
+               [?Y :last-name ?Z]]]
+            db)
+         '({:fname "Michael", :f "Kevin"}
+           {:fname "Kevin", :f "Kevin"}))))
