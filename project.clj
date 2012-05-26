2d8a8b1e92a68b70847b2068b21a202f0488a50e
Kevin Downey <redchin@gmail.com>
2012-05-25 17:25:54 -0700

    sql and lein2, I guess?

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject datalog "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]
-  :dev-dependencies [[criterium "0.2.1"]])
+  :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]
+                 [org.apache.derby/derby "10.8.2.2"]
+                 [org.clojure/java.jdbc "0.2.1"]]
+  :profiles {:dev {:dependencies [[criterium "0.2.1"]]}}
+  :plugins [[lein-swank "1.4.4"]])
